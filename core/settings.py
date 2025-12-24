2e2e781f3247d0713865af8dff22d815d781973a
RNT13 <renatornt13@gmail.com>
2025-12-24 08:37:50 -0300

    Fix: - Ajuste no DJANGO_ALLOWED_HOSTS

--- core/settings.py
+++ core/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 DEBUG = os.getenv("DEBUG", "1") == "1"
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS").split(",")
 CSRF_TRUSTED_ORIGINS = [origin.strip() for origin in os.getenv("DJANGO_CSRF_TRUSTED_ORIGINS", "").split(",") if origin]
 
 AUTH_USER_MODEL = "users.User"
