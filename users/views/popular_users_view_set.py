16b1514900ebedebe7cbf60a60fd38174aa3fbfc
RNT13 <renatornt13@gmail.com>
2026-01-01 16:57:41 -0300

    Update: - Melhorias no PopularUsersViewSet

--- users/views/popular_users_view_set.py
+++ users/views/popular_users_view_set.py
@@ -1,6 +1,8 @@
-from django.db.models import Count, F
+from django.db.models import Count, F, IntegerField, OuterRef, Subquery, Value
+from django.db.models.functions import Coalesce
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
+from posts.models import Post
 from users.models import User
 from users.serializers.user_serializer import UserSerializer
 
@@ -9,12 +11,18 @@ class PopularUsersViewSet(ReadOnlyModelViewSet):
     serializer_class = UserSerializer
 
     def get_queryset(self):
+        posts_count = Post.objects.filter(author=OuterRef("pk")).values("author").annotate(c=Count("id")).values("c")
+
+        likes_count = Post.objects.filter(author=OuterRef("pk")).annotate(c=Count("likes")).values("c")
+
+        comments_count = Post.objects.filter(author=OuterRef("pk")).annotate(c=Count("comments")).values("c")
+
         return (
             User.objects.annotate(
+                posts_count=Coalesce(Subquery(posts_count, output_field=IntegerField()), Value(0)),
+                likes_count=Coalesce(Subquery(likes_count, output_field=IntegerField()), Value(0)),
+                comments_count=Coalesce(Subquery(comments_count, output_field=IntegerField()), Value(0)),
                 followers_count=Count("followers", distinct=True),
-                posts_count=Count("posts", distinct=True),
-                likes_count=Count("posts__likes", distinct=True),
-                comments_count=Count("posts__comments", distinct=True),
             )
             .annotate(
                 popularity_score=F("followers_count") * 3
