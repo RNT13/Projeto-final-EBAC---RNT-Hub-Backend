afa261c58184e00ac573219d7fed24913eecaa53
RNT13 <renatornt13@gmail.com>
2026-01-01 17:31:28 -0300

    Fix: - Ajustes na urls.py de users

--- users/urls.py
+++ users/urls.py
@@ -2,18 +2,16 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from users.views.popular_users_view_set import PopularUsersViewSet
-from users.views.user_register_view_set import UserRegisterViewSet
 from users.views.user_view_set import UserViewSet
 
 router = DefaultRouter()
 router.register("", UserViewSet, basename="users")
-router.register("popular", PopularUsersViewSet, basename="popular-users")
 
 urlpatterns = [
     path(
-        "register/",
-        UserRegisterViewSet.as_view({"post": "create"}),
-        name="register",
+        "popular/",
+        PopularUsersViewSet.as_view({"get": "list"}),
+        name="popular-users",
     ),
 ]
 
