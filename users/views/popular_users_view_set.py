a7bcb52a91f11ca8c160ef7c069b3747adda18e1
RNT13 <renatornt13@gmail.com>
2026-01-01 16:41:45 -0300

    Update: - Melhorias no PopularUsersViewSet

--- users/views/popular_users_view_set.py
+++ users/views/popular_users_view_set.py
@@ -1,4 +1,4 @@
-from django.db.models import Count
+from django.db.models import Count, F
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from users.models import User
@@ -9,7 +9,19 @@ class PopularUsersViewSet(ReadOnlyModelViewSet):
     serializer_class = UserSerializer
 
     def get_queryset(self):
-        return User.objects.annotate(
-            followers_count=Count("followers", distinct=True),
-            posts_count=Count("posts", distinct=True),
-        ).order_by("-followers_count", "-posts_count")
+        return (
+            User.objects.annotate(
+                followers_count=Count("followers", distinct=True),
+                posts_count=Count("posts", distinct=True),
+                likes_count=Count("posts__likes", distinct=True),
+                comments_count=Count("posts__comments", distinct=True),
+            )
+            .annotate(
+                popularity_score=F("followers_count") * 3
+                + F("likes_count") * 2
+                + F("comments_count") * 3
+                + F("posts_count")
+            )
+            .filter(popularity_score__gt=0)
+            .order_by("-popularity_score", "-followers_count")
+        )
